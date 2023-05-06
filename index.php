ba9927c819bd957479a3526e09aa008ebf36022f
haidarcse2018 <haidarcse2017@gmail.com>
2023-05-06 20:37:20 +0600

    learing php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+
+// echo "My first PHP script!";
+
+echo  $para = "hello world";
+echo "<br/>";
+echo $x = 5;
+echo "<br/>";
+echo $y = 10.5;
+
+echo "<br/>";
+
+echo 5+5;
+echo "<br/>";
+echo $x +$y;
+?>
